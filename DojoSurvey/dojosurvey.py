21c7e625cc32616715ad8c1e194674fbfd852354
Jason <user@Jasons-MacBook-Pro.local>
2018-06-08 16:18:09 -0500

    second push

--- DojoSurvey/dojosurvey.py
+++ DojoSurvey/dojosurvey.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-app = Flask(name)
+from flask import Flask, render_template, redirect, request
+app = Flask(__name__)
 
 @app.route('/')
 def survey():
@@ -7,13 +7,12 @@ def survey():
 
 @app.route('/result',methods=['POST'])
 def result():
-
     return render_template("index2.html")
 
-@app.route('/danger')
+@app.route('/danger',methods=['GET'])
 def danger():
     print("a user tried to visit /danger. we have redirected the user to /")
-    return render_template("index.html")
+    return redirect('/')
 
 app.run(debug=True) 
 
